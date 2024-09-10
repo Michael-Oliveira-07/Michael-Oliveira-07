fd5573293b247e0f84e2954d7d90e3c487c75d89
Michael Oliveira <89768601+Michael-Oliveira-07@users.noreply.github.com>
2024-09-10 16:42:34 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Esse é meu Primeiro App.
+        </p>
+        <a
+          className="App-link"
+          href="http://comunidadecloud.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Cloud Treinamentos
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
